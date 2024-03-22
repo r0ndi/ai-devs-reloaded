39113f6b95dbc437c83e1680b916980ed3d019b5
ksadel <konrad.sadel@currency-one.com>
2024-03-22 08:59:47 +0100

    minor improvments in blogger task

--- src/tasks/blogger.ts
+++ src/tasks/blogger.ts
@@ -5,25 +5,28 @@ type BloggerData = TaskResponse & {
   blog: string[]
 }
 
+type Completions = {
+  choices: { message: { content: string } }[]
+}
+
 export async function blogger({ blog }: BloggerData): Promise<string[]> {
+  return Promise.all(blog.map(createCompletions))
+}
+
+async function createCompletions(content: string): Promise<string> {
   const systemContext =
     `#Role: Food blogger.
      #Instraction: Return only a few sentences (max 4) on a given topic. Return information on history, recipes,
      creation process and tips. Answer must be expressed in the same language as the question`
 
   const openai = new OpenAI()
-  const response: string[] = []
-
-  for (const sentence of blog) {
-    const completion = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        { role: 'system', content: systemContext },
-        { role: 'user', content: sentence },
-      ],
-    })
-    response.push(completion.choices[0].message.content as string)
-  }
-
-  return response
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      { role: 'system', content: systemContext },
+      { role: 'user', content },
+    ],
+  }) as Completions
+  
+  return completion.choices[0].message.content
 }
