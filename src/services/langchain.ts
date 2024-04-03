f451b3cf2df9e006f7aafb4d05ce5e472dcaceab
ksadel <konrad.sadel@currency-one.com>
2024-04-03 17:35:35 +0200

    change default llm model

--- src/services/langchain.ts
+++ src/services/langchain.ts
@@ -3,7 +3,7 @@ import { HumanMessage, SystemMessage } from '@langchain/core/messages'
 import { Model } from '../types/local'
 
 export async function invoke(
-  systemMessage: string, humanMessage: string, options?: ChatOpenAICallOptions, model: Model = Model.GPT_4,
+  systemMessage: string, humanMessage: string, options?: ChatOpenAICallOptions, model: Model = Model.GPT_3_5_TURBO,
 ): Promise<string> {
   const openAi = new ChatOpenAI({ modelName: model })
   const response = await openAi.invoke([
