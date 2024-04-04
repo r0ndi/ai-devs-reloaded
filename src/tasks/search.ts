507f7e45a2a1ea5157b21541572c80d6895fdeef
ksadel <konrad.sadel@currency-one.com>
2024-04-04 16:52:58 +0200

    search task

---
+++ src/tasks/search.ts
@@ -0,0 +1,65 @@
+import * as langchainService from '../services/langchain'
+import * as qdrantService from '../services/qdrant'
+import { TaskResponse } from '../types/remote'
+import { Document } from 'langchain/document'
+import { QdrantItem } from '../types/local'
+import axios, { AxiosError } from 'axios'
+import { v4 } from 'uuid'
+
+type SearchData = TaskResponse & {
+  question: string
+}
+
+type News = {
+  title: string
+  url: string
+  info: string
+  date: string
+}
+
+type SearchItem = {
+  payload: { url: string }
+}
+
+const NEWS_URL = 'https://unknow.news/archiwum_aidevs.json'
+const COLLECTION_NAME = 'news'
+
+export async function handler({ question }: SearchData): Promise<string> {
+  const client = qdrantService.createClient()
+
+  if (!await qdrantService.isCollectionExists(client, COLLECTION_NAME)) {
+    await qdrantService.createCollection(client, COLLECTION_NAME, true)
+  }
+
+  if (await qdrantService.isEmptyCollection(client, COLLECTION_NAME)) {
+    const news = await getNews()
+    const documents = await prepareQdrantItems(news)
+    await qdrantService.upsertItems(client, COLLECTION_NAME, documents)
+  }
+
+  const queryEmbedding = await langchainService.getEmbedding(question)
+  const { payload: { url } } = await qdrantService.searchItem<SearchItem>(client, COLLECTION_NAME, queryEmbedding)
+  return url || ''
+}
+
+async function prepareQdrantItems(news: News[]): Promise<QdrantItem[]> {
+  const prepareItem = async ({ info, url, title }: News): Promise<QdrantItem> => {
+    const document = new Document({ pageContent: info })
+    return {
+      id: v4(),
+      payload: { ...document.metadata, content: info, title, url },
+      vector: await langchainService.getEmbedding(document.pageContent),
+    }
+  }
+  return Promise.all(news.map(prepareItem))
+}
+
+async function getNews(): Promise<News[]> {
+  try {
+    const { data } = await axios.get(NEWS_URL)
+    return data as News[]
+  } catch (error: unknown) {
+    const { message } = error as AxiosError
+    throw new Error(`Error during file fetching: ${message}. Run again!`)
+  }
+}
