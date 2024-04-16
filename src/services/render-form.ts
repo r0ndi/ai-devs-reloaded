1dd2583b2daa3a2b84c393966492ba0eb8059624
ksadel <konrad.sadel@currency-one.com>
2024-04-16 08:06:32 +0200

    meme task

--- src/services/render-form.ts
+++ src/services/render-form.ts
@@ -4,20 +4,12 @@ import axios from 'axios'
 export async function generateImage(data: Record<string, string>): Promise<RenderFormResponse> {
   const response = await axios.post(
     process.env.RENDER_FORM_API_URL || '',
-    getTemplateData(data),
+    { template: process.env.RENDER_FORM_TEMPLATE_ID || '', data },
     { headers: getHeaders() },
   )
   return response.status === 200
     ? { success: true, ...response.data }
     : { success: false }
-}
-
-function getTemplateData(data: Record<string, string>): Record<string, string | Record<string, string>> {
-  const templateId = process.env.RENDER_FORM_TEMPLATE_ID || ''
-  return {
-    template: templateId,
-    data,
-  }
 }
 
 function getHeaders(): Record<string, string> {
